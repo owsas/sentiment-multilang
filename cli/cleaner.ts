ed7f523cce22cd6e2a310dbdae265ef2b661868a
Juan Camilo Guarin Peñaranda <jcguarinpenaranda@iMac-de-Juan.local>
2018-09-07 07:16:35 -0500

    created cleaner cli program to clean any given json document and export it to a new json file

---
+++ cli/cleaner.ts
@@ -0,0 +1,22 @@
+/**
+ * Script that removes duplicate keys and orders alphabetically
+ * any given json language
+ */
+import * as program from 'commander';
+import * as path from 'path';
+import * as fs from 'fs';
+import { clean } from '../lib/clean';
+const { version } = require('../package.json');
+
+program.version(version)
+  .option('-i --input [value]')
+  .parse(process.argv);
+
+// Read the document
+const document = require(path.resolve(program.input));
+
+// Clean the object
+const newObject = clean(document);
+
+// Write the out file
+fs.writeFileSync(path.resolve(__dirname, 'out.json'), JSON.stringify(newObject, null, 2), 'utf8');

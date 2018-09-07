b6fd7ec67af928f73e40e0eaf1fd82de14cfac27
Juan Camilo Guarin Peñaranda <jcguarinpenaranda@iMac-de-Juan.local>
2018-09-07 07:21:01 -0500

    tests: formatted

--- test/test.js
+++ test/test.js
@@ -9,31 +9,31 @@ var assert = require('assert');
 // Require the sentiment module
 var sentiment = require('../index');
 
-describe('English', function() {
-    it('It should return positive or negative', function() {
-        assert.equal('negative', sentiment('Cats are stupid.', 'en').vote);
-        assert.equal('positive', sentiment('Cats are totally amazing!', 'en').vote);
-    });
+describe('English', function () {
+  it('It should return positive or negative', function () {
+    assert.equal('negative', sentiment('Cats are stupid.', 'en').vote);
+    assert.equal('positive', sentiment('Cats are totally amazing!', 'en').vote);
+  });
 });
 
-describe('Italian', function() {
-    it('It should return positive or negative', function() {
-        assert.equal('positive', sentiment('Il mare è bello', 'it').vote);
-        assert.equal('negative', sentiment('I gatti sono stupidi.','it').vote);
-    });
+describe('Italian', function () {
+  it('It should return positive or negative', function () {
+    assert.equal('positive', sentiment('Il mare è bello', 'it').vote);
+    assert.equal('negative', sentiment('I gatti sono stupidi.', 'it').vote);
+  });
 });
 
-describe('Spanish', function() {
-    it('It should return positive or negative', function() {
-        assert.equal('negative', sentiment('Los gatos son estúpidos.', 'es').vote);
-        assert.equal('positive', sentiment('Los gatos son totalmente increíble!', 'es').vote);
-    });
+describe('Spanish', function () {
+  it('It should return positive or negative', function () {
+    assert.equal('negative', sentiment('Los gatos son estúpidos.', 'es').vote);
+    assert.equal('positive', sentiment('Los gatos son totalmente increíble!', 'es').vote);
+  });
 });
 
-describe('Wrong language', function() {
-    it('It should return positive, negative or neutral', function() {
-        assert.equal('positive', sentiment('Seems somebody had a good meal! #lion #safari #cats #wildlife #Africa #adventure #offroad https://t.co/6cX7hAlrYY', 'en').vote);
-        assert.equal('neutral', sentiment('Seems somebody had a good meal! #lion #safari #cats #wildlife #Africa #adventure #offroad https://t.co/6cX7hAlrYY', '8g8u').vote);
-        assert.equal('neutral', sentiment('Seems somebody had a good meal! #lion #safari #cats #wildlife #Africa #adventure #offroad https://t.co/6cX7hAlrYY', 'it').vote);
-    });
+describe('Wrong language', function () {
+  it('It should return positive, negative or neutral', function () {
+    assert.equal('positive', sentiment('Seems somebody had a good meal! #lion #safari #cats #wildlife #Africa #adventure #offroad https://t.co/6cX7hAlrYY', 'en').vote);
+    assert.equal('neutral', sentiment('Seems somebody had a good meal! #lion #safari #cats #wildlife #Africa #adventure #offroad https://t.co/6cX7hAlrYY', '8g8u').vote);
+    assert.equal('neutral', sentiment('Seems somebody had a good meal! #lion #safari #cats #wildlife #Africa #adventure #offroad https://t.co/6cX7hAlrYY', 'it').vote);
+  });
 });

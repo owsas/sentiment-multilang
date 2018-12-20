df049e70eb9a682fe96f32aef5c09bef0b37cd52
Rolando Santamaria Maso <kyberneees@gmail.com>
2018-12-20 22:49:42 +0100

    improving clean function

--- src/lib/clean.ts
+++ src/lib/clean.ts
@@ -1,15 +1,17 @@
+/**
+ * Generate a new clean object with all keys in lowercase (no-duplicates)
+ * 
+ * @param {Object} object 
+ */
 export function clean(object: {[k: string]: any}): {[k: string]: any} {
-  // Order alphabetically
-  const sortedKeys = Object.keys(object).sort();
-  
-  // Clean all duplicate keys
-  const unique = new Set(sortedKeys);
-  
-  // Generate the new clean object with all keys in lowercase 
-  const newObject = {};
-  Array.from(unique).forEach((key) => {
-    newObject[key.toLowerCase()] = object[key];
-  });
-
-  return newObject;
+  return Object.keys(object).sort().reduce((container, key) => {
+    // Get all keys as lowercase
+    const lowerKey = key.toLowerCase();
+    // Generate a new clean object with all keys in lowercase (no-duplicates)
+    if (undefined === container[lowerKey]) {
+      container[lowerKey] = object[key];
+    }
+    
+    return container;
+  }, {});
 }
